b7a8b62f9dae5201359b476f26fc5e0b9498ddd8
KennethMasis <Kjmasis23@gmail.com>
2024-07-18 13:53:08 -0600

    Add files via upload

---
+++ new.js
@@ -0,0 +1,12 @@
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+document.getElementById('loginForm').addEventListener('submit', function(event) {
+    event.preventDefault();
+    // Aquí puedes añadir validación o autenticación si lo deseas
+    window.location.href = 'Dulce_amor.html';
+});
+
+
